06078a976c550c63438a08c397c3fbf08a72259f
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-09-07 20:48:33 +0800

    .

--- Axie_marketplace/settings.py
+++ Axie_marketplace/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dict2.1.teamone@gmail.com'
-EMAIL_HOST_PASSWORD = 'KzGB9n*i*^JVBzQ9'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
